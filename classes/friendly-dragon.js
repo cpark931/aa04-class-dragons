3c482ae8ad13d4aed9702e506c5a21ffe5b91caa
sybil <jordan.sybil@hotmail.com>
2024-03-07 22:20:16 -0800

    almost done

--- classes/friendly-dragon.js
+++ classes/friendly-dragon.js
@@ -16,4 +16,32 @@ class FriendlyDragon extends Dragon{
          return `${this.name} helps their friend ${this.friend}`
       }
 }
+const puff = new FriendlyDragon(
+      "Puff",
+      "green",
+      [
+        "live by the sea",
+        "frolick in the autumn mist",
+        "help small children"
+      ],
+      "Jackie Paper"
+    );
+    console.log(puff);
+    console.log(puff.breathesFire()); //Friendly dragons can still breathe fire!
+    puff.hasLifeGoals();
+    console.log(puff.helpsPeople());
+
+    const toothless = new FriendlyDragon(
+      "Toothless",
+      "black",
+      [
+        "save the town of Burke",
+        "fly with a kid on his back",
+        "hang out with Vikings"
+      ],
+      "Hiccup"
+    );
+    console.log(toothless); toothless.hasLifeGoals();
+    console.log(toothless.helpsPeople());
+    console.log(toothless.breathesFire());
 module.exports = FriendlyDragon;
